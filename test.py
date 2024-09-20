ff565b67843a089c63436539efb98612864ba624
lDavidSantiago <david.triana@correounivalle.edu.co>
2024-09-20 00:28:17 -0500

    esto esta muy dificil

--- test.py
+++ test.py
@@ -20,45 +20,42 @@ def kill(word1,word2,i,j):
 advance_Cost = 1
 delete_Cost = 1
 replace_Cost = 2
-insert_Cost = 1
-kill_Cost = 4
+insert_Cost =2
+kill_Cost = 2
 #
 def calcular_mejor_coste(word1,word2,i,j,advance_Cost,delete_Cost,replace_Cost,insert_Cost,kill_Cost,operaciones,costos):
     if i == len(word1) and j == len(word2):
         return costos,operaciones
     if i == len(word1):
         word1,word2,i,j = insert(word1,word2,i,j)
-        operaciones.append('insert '+ str(word1))
+        operaciones.append('insert ')
         costos.append(insert_Cost)
         return calcular_mejor_coste(word1,word2,i,j,advance_Cost,delete_Cost,replace_Cost,insert_Cost,kill_Cost,operaciones,costos)
     if j == len(word2):
-        print("palabras que faltan por borrar" + str(word1[i:]))
+        
         to_delete = len(word1[i:])
         if to_delete*delete_Cost<kill_Cost:        
             word1,word2,i,j = delete(word1,word2,i,j)
-            operaciones.append('delete '+ str(word1))
+            operaciones.append('delete ')
             costos.append(delete_Cost)
             return calcular_mejor_coste(word1,word2,i,j,advance_Cost,delete_Cost,replace_Cost,insert_Cost,kill_Cost,operaciones,costos)
         else:
             word1,word2,i,j = kill(word1,word2,i,j)
-            operaciones.append('kill '+ str(word1))
+            operaciones.append('kill ')
             costos.append(kill_Cost)
             return calcular_mejor_coste(word1,word2,i,j,advance_Cost,delete_Cost,replace_Cost,insert_Cost,kill_Cost,operaciones,costos)
     if word1[i] == word2[j]:
         word1,word2,i,j = advance(word1,word2,i,j)
-        operaciones.append('advance '+ str(word1))
+        operaciones.append('advance ')
         costos.append(advance_Cost)
         return calcular_mejor_coste(word1,word2,i,j,advance_Cost,delete_Cost,replace_Cost,insert_Cost,kill_Cost,operaciones,costos)
-    
+costos1 = []
+costos2 = []
 operaciones = []
 operaciones2 = []
 word1 = ['a','b']
 word2 = ['a','b','c','s']
-calcular_mejor_coste(word1[:],word2[:],0,0,advance_Cost,delete_Cost,replace_Cost,insert_Cost,kill_Cost,operaciones,[])
-calcular_mejor_coste(word2,word1,0,0,advance_Cost,delete_Cost,replace_Cost,insert_Cost,kill_Cost,operaciones2,[])
-for op in operaciones:
-    print(op)
-print('----------------------------------')
-for op in operaciones2:
-    print(op)
+calcular_mejor_coste(word1[:],word2[:],0,0,advance_Cost,delete_Cost,replace_Cost,insert_Cost,kill_Cost,operaciones,costos1)
+calcular_mejor_coste(word2,word1,0,0,advance_Cost,delete_Cost,replace_Cost,insert_Cost,kill_Cost,operaciones2,costos2)
+print(operaciones2)
 
